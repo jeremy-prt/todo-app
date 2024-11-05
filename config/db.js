b4d1ca36d6cab838eb6816d275322f7b7d7fe963
Jeremy <jeremyperret1@gmail.com>
2024-11-05 15:28:54 +0100

    first commit

---
+++ config/db.js
@@ -0,0 +1,34 @@
+// FILE: config/db.js
+const mysql = require("mysql2");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+});
+
+function sqlQuery(query) {
+  return new Promise((resolve, reject) => {
+    pool.getConnection((err, connection) => {
+      if (err) {
+        reject(err);
+        return;
+      }
+      connection.query(query, (err, result) => {
+        connection.release();
+        if (err) {
+          reject(err);
+        } else {
+          resolve(result);
+        }
+      });
+    });
+  });
+}
+
+module.exports = sqlQuery;
